6301cbf81250a25e33bca48dc1fb5bec2f11051f
TSIBSPROC\romulo.teixeira <romulo.teixeira@sibs.com>
2020-06-02 15:02:23 +0100

    New version of implementation of IdentityServer4 with ASP.Net Identity

---
+++ IdentityServer4.Deblokt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IdentityServer4.Deblokt.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityServer4.Deblokt.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityServer4.Deblokt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDbContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("IdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityDbContext>();
+            });
+        }
+    }
+}
---
+++ IdentityServer4.Deblokt/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer4.Deblokt.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
